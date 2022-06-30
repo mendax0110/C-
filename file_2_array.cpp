b68e38f67843279f249fdc722b900b308ba67fa3
m3ndax <adrian.goessl@outlook.com>
2022-06-30 19:03:25 +0200

    update code and comment

--- file_2_array.cpp
+++ file_2_array.cpp
@@ -1,19 +1,30 @@
+/*include the libraries*/
 #include <iostream>
-using namespace std;
-//this function adds the corresponding 
-//elements of both the arrays and displays it
 
-void sum(int arr[], int arr2[]) {
+using namespace std;
+
+/*this function adds the corresponding 
+ *elements of two arrays and displays it
+ */
+void sum(int arr1[], int arr2[])
+{
     int temp[5];
-    for(int i = 0; i < 5; i++) {
-        temp[i] = arr1[i]+arr2[i];
+
+    for(int i = 0; i < 5; i++)
+    {
+        temp[i] = arr1[i] + arr2[i];
+        /*display the sum of the two arrays*/
         cout << temp[i] << endl;
     }
 }
-int main() {
+
+/*main function*/
+int main()
+{
     int a[5] = {10, 20, 30, 40, 50};
     int b[5] = {1, 2, 3, 4, 5};
     sum(a, b);
-    //passing array to function
+
+    /*passing array to function*/
     return 0;
 }
