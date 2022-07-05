d3794893e208a197da4ae83d1564325b36b52d78
m3ndax <adrian.goessl@outlook.com>
2022-07-05 22:05:49 +0200

    update code and comment

--- array_wave.cpp
+++ array_wave.cpp
@@ -1,37 +1,57 @@
+/*include the libraries*/
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-void swap(int * p, int * q) {
-    int temp = * p;
-    * p = * q;
-    * q = temp;
+/*void function*/
+void swap(int * p, int * q)
+{
+    int temp = *p;
+    *p = *q;
+    *q = temp;
 }
 
-void array_in_wave(int array[], int n)  {
+/*void array in wave*/
+void array_in_wave(int array[], int n)
+{
     sort(array, array + n);
-    for (int i = 0; i < n -1; i += 2)
-        swap( & array[i], & array[i + 1]);
+
+    for(int i = 0; i < n -1; i += 2)
+    {
+         swap(&array[i], &array[i + 1]);
+    }
 }
 
-int main()  {
+/*main function*/
+int main()
+{
     int array[100], n, i;
-    cout << "Enter number of elements: ";
+
+    /*input the number of elements*/
+    cout << "Enter the number of elements: ";
     cin >> n;
-    cout << "\nEnter elements: ";
+    cout << "\nEnter the elements: ";
 
-    for (i = 0; i < n; i++)
-    cin >> array[i];
-    cout << "Original array: ";
+    for(i = 0; i < n; i++)
+    {
+        cin >> array[i];
+        cout << "Original array: ";
+    }  
 
-    for (int i = 0; i < n; i++)
-    cout << aray[i] << " ";
+    for(int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
+    }   
 
     array_in_wave(array, n);
+    /*output the array*/
     cout << "\nWave form of the array is: ";
 
-    for (int i = 0; i < n; i++)
-    cout << array[i] << " ";
-
+    for(int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
+    }
+        
     return 0;
 }
