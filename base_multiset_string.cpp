6531b55bbf1f2d86e52aa8694423438da437ceae
m3ndax <adrian.goessl@outlook.com>
2022-07-05 21:55:37 +0200

    update code and comment

--- base_multiset_string.cpp
+++ base_multiset_string.cpp
@@ -1,34 +1,41 @@
+/*include the libraries*/
 #include <iostream>
-#include <set>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
+/*main function*/
 int main()
 {
-    int a[ 10 ] = { 7, 22, 9, 1, 18, 30, 100, 22, 85, 13 };
+    int a[10] = {7, 22, 9, 1, 18, 30, 100, 22, 85, 13};
     int aSize = sizeof(a) / sizeof(int);
-    std::mulitset< int, std::less< int > > intMultiset(a, a + aSize);
 
-    cout << "Printing out all the values in the multiset : ";
+    std::multiset<int, std::less<int> > intMultiset(a, a + aSize);
+
+    /*print the elements of the multiset*/
+    cout << "Printing the elements of the multiset: ";
     multiset<int>::iterator it;
-    for ( it = intMultiset.begin(); int != intMultiset.end(); ++it)
+
+    for(it = intMultiset.begin(); it != intMultiset.end(); ++it)
         cout << " " << *it;
     cout << endl << endl;
 
-    std::ostream_iterator< int > output ( cout, " ");
+    std::ostreambuf_iterator<int> output(cout, " ");
 
-    cout << "There are currently " << intMultiset.cout( 15 )
-         << "values of 15 in the multiset\n\n";
-    
-    intMultiset.insert( 15 );
-    intMultiset.insert( 15 );
+    /*print the elements of the multiset*/
+    cout << "There are currently " << intMultiset.count(15) << " elements in the multiset\n\n";
 
-    cout << "After two inserts, there are currently " << intMultiset.cout( 15 )
-         << "values of 15 in the multiset\n\n";
+    intMultiset.insert(15);
+    intMultiset.insert(15);
 
-    cout << "Printing out all the values in the multiset : ";
-    for ( it = intMultiset.begin(); it != intMultiset.end(); ++it)
+    /*print the elements of the multiset*/
+    cout << "After two inserts, there are currently " << intMultiset.count(15) << " elements of 15 in the multiset\n\n";
+
+    /*print the elements of the multiset*/
+    cout << "Printing the elements of the multiset: ";
+
+    for(it = intMultiset.begin(); it != intMultiset.end(); ++it)
         cout << " " << *it;
     cout << endl << endl;
 
