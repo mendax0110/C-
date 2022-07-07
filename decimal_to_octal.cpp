6d99e98a6a6c529be2a037eeff38ac889d211688
m3ndax <adrian.goessl@outlook.com>
2022-07-07 10:30:18 +0200

    update code and comment

--- decimal_to_octal.cpp
+++ decimal_to_octal.cpp
@@ -1,26 +1,33 @@
+/*include the library*/
 #include <iostream>
 
 using namespace std;
 
+/*main int function*/
 int main()
 {
-    long dno, temp;
     int octal[100], i = 1, j;
+    long dno, temp;
 
+    /*input the decimal number*/
     cout << "Enter the decimal number: ";
     cin >> dno;
     temp = dno;
 
-    while (temp != 0)
+    /*convert the decimal number to octal*/
+    while(temp != 0)
     {
         octal[i++] = temp % 8;
-        temp = temp / 8;
+        temp /= 8;
     }
 
-    cout << "Equivalent octal value of decimal no: ";
+    /*output the octal number*/
+    cout << "Equivalent octal number is: ";
 
-    for (j = i - 1; j > 0; j--)
+    for(j = i - 1; j > 0; j--)
+    {
         cout << octal[j];
-    
-    return 0;
+    }
+
+    return 0;   
 }
