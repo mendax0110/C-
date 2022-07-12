14631e76fc6677247a7f2ad356f4bbb681b576b1
m3ndax <adrian.goessl@outlook.com>
2022-07-12 19:59:59 +0200

    update code and comment

--- insert_array.cpp
+++ insert_array.cpp
@@ -1,21 +1,35 @@
+/*include the library*/
 #include <iostream>
+
 using namespace std;
 
+/*main function*/
 int main()
 {
     int arr[6], i, elem;
-    cout << "Enter 5 Array Elements: ";
-    for (i = 0; i < 5; i++)
-        cin >> arr[i];
 
-    cout << "\nEnter Element to Insert: ";
+    /*insert the arrays*/
+    cout << "Enter max 5 Array elements: ";
+
+    for(i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    /*insert the element*/
+    cout << "\nEnter the element to be inserted: ";
     cin >> elem;
     arr[i] = elem;
 
-    cout << "\nThe New Array is: \n";
-    for (i = 0; i < 6; i++)
-        cout << arr[i] << "  ";
-    cout << endl;
+    /*display the array*/
+    cout << "\nThe array after insertion is: \n";
+
+    for(i = 0; i < 6; i++)
+    {
+        cout << arr[i] << " ";
+        cout << endl;
+    }
 
     return 0;
+
 }
