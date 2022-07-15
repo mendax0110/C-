8ece5184eae982d822455ef80d971f1325508d57
m3ndax <adrian.goessl@outlook.com>
2022-07-15 14:16:22 +0200

    update code and comment

--- pattern_positon.cpp
+++ pattern_positon.cpp
@@ -1,19 +1,28 @@
+/*include the library*/
 #include <iostream>
+
 using namespace std;
 
+/*main function*/
 int main()
 {
     int n;
-    cout << "Enter a number of rows to be printed\n"; 
+
+    /*input the number*/
+    cout << "Enter the number of rows to be printed\n";
     cin >> n;
 
-    for (int i = 1; i < n; ++i) 	//for each row
+    for(int i = 1; i < n; ++i) /*for each row*/
     {
-        for (int j = 0; j < n - 1; ++j) //for printing spaces
+        for(int j = 0; j < n - 1; ++j) /*for printing spaces*/
+        
             cout << " ";
-        for (int j = 0; j < 2 * i - 1; ++j) //for printing star
+        
+        for(int j = 0; j < 2 * i -1; ++j) /*for printing stars*/
+        
             cout << " * ";
-            cout << "\n";   //going to new line after completing one row
+            cout << "\n"; /*go to new line after each row*/     
     }
+    
     return 0;
 }
