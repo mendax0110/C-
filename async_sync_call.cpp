2bad438a46e9fcb638bbe27af5ecd34241c249e7
m3ndax <adrian.goessl@outlook.com>
2022-07-18 13:10:33 +0200

    update code and comment

--- async_sync_call.cpp
+++ async_sync_call.cpp
@@ -1,48 +1,56 @@
+/*include the libraries*/
+#include <iostream>
 #include <windows.h>
 #include <process.h>
-#include <iostream>
 
 using namespace std;
 
+/*declare the functions*/
 void Func1(void *);
 void Func2(void *);
 
-CRITICAL_SECTION Section;   // This will act as Mutex
+/*this will act as Mutex*/
+CRITICAL_SECTION Section; 
 
+/*main function*/
 int main()
 {
     InitializeCriticalSection(&Section);
 
-    //Synchronous calling
-    cout << "\nSynchronous calling" << endl;
+    /*syncronous call*/
+    cout << "\nSyncronous calling" << endl;
     Func1(0);
     Func2(0);
 
-    //Asynchronous calling
-    cout << "\nAsynchronous calling" << endl;
+    /*asyncronous call*/
+    cout << "\nAsyncronous calling" << endl;
     HANDLE hThreads[2];
 
-    //Create two threads and start them
+    /*create the threads and start them*/
     hThreads[0] = (HANDLE)_beginthread(Func1, 0, NULL);
     hThreads[1] = (HANDLE)_beginthread(Func2, 0, NULL);
 
-    //Make sure that both the threads have finished before going further
+    /*make sure that both the threads have finished before going further*/
     WaitForMultipleObjects(2, hThreads, TRUE, INFINITE);
 
-    //This is done after all threads have finished processing
+    /*this is done after all threads have finished processing*/
     DeleteCriticalSection(&Section);
 
-    cout << "Main exit" << endl;
+    /*exit the program*/
+    cout << "Main exiting" << endl;
+
     return 0;
 }
 
+/*function 1*/
 void Func1(void *P)
 {
     int Count;
 
-    for (Count = 10; Count > 0; Count--)
+    for(Count = 0; Count > 0; Count--)
     {
         EnterCriticalSection(&Section);
+        /*print the message*/
         cout << "Func2 loop" << Count << endl;
         LeaveCriticalSection(&Section);
         Sleep(1000);
