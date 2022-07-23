6039d0e6a9cdde6fbbb2481ec02fb0cb5e1729b7
m3ndax <adrian.goessl@outlook.com>
2022-07-23 22:53:41 +0200

    update code and comment

--- swapnumber.cpp
+++ swapnumber.cpp
@@ -1,21 +1,27 @@
+/*include the library*/
 #include <iostream>
+
 using namespace std;
 
+/*declare the function*/
 void cyclicSwap(int *a, int *b, int *c);
 
+/*main function*/
 int main()
 {
     int a, b, c;
 
+    /*input the value*/
     cout << "Enter value of a, b and c respectively: ";
     cin >> a >> b >> c;
 
+    /*call the function*/
     cout << "Value before swapping: " << endl;
     cout << "a, b and c respectively are: " << a << ", " << b << ", " << c << endl;
-
+    
     cyclicSwap(&a, &b, &c);
-
-    cout << "Value after swapping number in cycle: " << endl;
+    /*call the function*/
+    cout << "Value after swapping number in cyclic manner: " << endl;
     cout << "a, b and c respectively are: " << a << ", " << b << ", " << c << endl;
 
     return 0;
@@ -29,3 +35,4 @@ void cyclicSwap(int *a, int *b, int *c)
     *a = *c;
     *c = temp;
 }
+
