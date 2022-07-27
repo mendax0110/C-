293416638bb6f15fc37cd361df48770f4c096ade
m3ndax <adrian.goessl@outlook.com>
2022-07-27 20:45:41 +0200

    update code and comment

--- param_constructor.cpp
+++ param_constructor.cpp
@@ -1,17 +1,25 @@
+/*include the library*/
 #include <iostream>
+
 using namespace std;
 
-class Add{
+class Add
+{
     public:
-        //Parameterized constructor
-        Add(int num1, int num2) {
-            cout << (num1 + num2) << endl;
-        }
+    /*parameterized constructor*/
+    Add(int num1, int num2)
+    {
+        cout << (num1 + num2) << endl;
+    }
 };
-int main(void){
-    //implicit call to the constructor
+
+/*main function*/
+int main(void)
+{
+    /*implicit call to the constructor*/
     Add obj1(10, 20);
-    //is known as explicit calling the constructor
-    Add obj2 = Add(50, 60);
+    /*is known as explicit call to the constructor*/
+    Add obj2(50, 60);
+
     return 0;
 }
