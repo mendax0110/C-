4bbd255324ac182b670f0aa06b805faf05534f60
m3ndax <adrian.goessl@outlook.com>
2022-07-28 21:16:23 +0200

    update code and comment

--- nth_bit.cpp
+++ nth_bit.cpp
@@ -1,17 +1,19 @@
+/*include the library*/
 #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
+/*main function*/
 int main()
 {
     int num, n;
 
-    cout << "Enter number\n";
+    cout << "Enter the number: ";
     cin >> num;
 
-    cout << "Enter bit number you wish to obtain\n";
+    cout << "Enter the bit number you wish to obtain: ";
     cin >> n;
 
-    cout << "Answer:" << (1 &(num >> (n - 1)));
+    cout << "Answer: " << (1 & (num >> (n - 1)));
 }
