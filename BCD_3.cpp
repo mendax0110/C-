ef42d395ab399ca8d1113b8ad19132ce5d60345f
m3ndax <adrian.goessl@outlook.com>
2022-08-06 13:22:55 +0200

    number BCD converter

---
+++ BCD_3.cpp
@@ -0,0 +1,47 @@
+/*include the library*/
+#include <iostream>
+
+/*define the main function*/
+#define uint unsigned int
+
+using namespace std;
+
+uint makeReverseBCD(uint num);
+
+/*main function*/
+int main()
+{
+    uint n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "BCD is: " << makeReverseBCD(n) << endl;
+}
+
+/*function to make the reverse BCD*/
+uint reverseBinary(uint num)
+{
+    uint result = 0;
+
+    for(uint digits = 4; digits > 0; num /= 2, --digits)
+    {
+        result = result * 10 + num % 2;
+    }
+
+    return result;
+}
+
+/*using the normal operator and uint
+ *using bitwise operator
+ */
+uint makeReverseBCD(uint num)
+{
+    uint BCD = 0;
+
+    for(; num > 0; num /= 10)
+    {
+        BCD = BCD * 10000 + reverseBinary(num % 10 + 3);
+    }
+
+    return BCD;
+}
+
