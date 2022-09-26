0597f260134e4c0b99746ef37d2e478000c471c7
m3ndax <adrian.goessl@outlook.com>
2022-09-26 19:38:33 +0200

    update code and comment

--- matrix/sym_skew_sym_matrix.cpp
+++ matrix/sym_skew_sym_matrix.cpp
@@ -1,66 +1,85 @@
+// include the header files
 #include <iostream>
 
 using namespace std;
 
+// main function
 int main()
 {
     int n;
-    cout << "Enter size of square matrix\n";    //inputting size of the matrix
+
+    // read the size of the matrix
+    cout << "Enter the size of the square-matrix:\n";
     cin >> n;
 
     int a[n][n];
-    cout << "Enter the matrix row-wise\n";  //inputting the matrix row wise
-    for (int i = 0; i < n; ++i)
+
+    // read the elements of the matrix
+    cout << "Enter the matrix row-wise:\n";
+
+    for(int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; ++j)
+        for(int j = 0; j < n; j++)
         {
             cin >> a[i][j];
         }
     }
 
     int ctr = 1;
-    for (int i = 0; i < n; ++i)     //check for symmetric matrix
+    // check if the matrix is symmetric
+    for(int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; ++j)
+        for(int j = 0; j < n; j++)
         {
-            if (a[i][j] != -a[j][i])
+            if(a[i][j] != a[j][i])
             {
                 ctr = 0;
                 break;
             }
         }
 
-        if (ctr == 0)
-        break;
+        if(ctr == 0)
+        {
+            break;
+        }
     }
 
-
-
-    if (ctr)    //printing if matrix is symmetric
-        cout << "Matrix is skew-symmetric\n";
-    else            //checking if skew-symmetric
+    if(ctr)
+    {
+        // the matrix is symmetric
+        cout << "The matrix is symmetric.\n";
+    }
+    else
     {
         ctr = 1;
-        for (int i = 0; i < n; ++i)
+
+        for(int i = 0; i < n; ++i)
         {
-            for (int j = 0; j < n; ++j)
+            for(int j = 0; j < n; ++j)
             {
-                if (a[i][j] != (-a[j][i]))
+                if(a[i][j] != -a[j][i])
                 {
                     ctr = 0;
                     break;
                 }
             }
 
-            if (ctr == 0)
-            break;
+            if(ctr == 0)
+            {
+                break;
+            }
         }
-
-        if (ctr)    //printing if matrix is skew symmetric matrix
-            cout << "Matrix is skew-symmetric\n";
-        else        //if not then its neither of the two
-            cout << "Matrix is neither symmetric nor skew-symmetric\n";
     }
 
+    if(ctr)
+    {
+        // the matrix is skew-symmetric
+        cout << "The matrix is skew-symmetric.\n";
+    }
+    else
+    {
+        // the matrix is neither symmetric nor skew-symmetric
+        cout << "The matrix is neither symmetric nor skew-symmetric.\n";
+    }
     return 0;
 }
