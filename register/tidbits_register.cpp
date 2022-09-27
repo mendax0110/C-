9e392fad4b855b5d8ec6ef01b6d4b292893128dd
m3ndax <adrian.goessl@outlook.com>
2022-09-27 20:00:35 +0200

    update code and comment

--- register/tidbits_register.cpp
+++ register/tidbits_register.cpp
@@ -1,16 +1,21 @@
+// include the header file
 #include <iostream>
 
+// using namespace std
 using namespace std;
 
+// main function
 int main()
 {
-    int i = 123;
-    auto int j = 456;
-    register int k = 789;
+    int i          = 123;
+    auto int j     = 456;
+    register int k = 789;	
 
-    cout<<"Address of i = " << &j << ", Value = " << j << endl;
-    cout<<"Address of j = " << &j << ", Value = " << j << endl;
-    cout<<"Address of k = " << &k << ", Value = " << k << endl;
+    // print the values
+    cout << "Address of i = " << &j << ", Value = " << j << endl;
+    cout << "Address of j = " << &j << ", Value = " << j << endl;
+    cout << "Address of k = " << &k << ", Value = " << k << endl;
 
+    // return 0
     return 0;
 }
