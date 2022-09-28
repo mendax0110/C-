f8b12c339f6fcdbbc0ef46e9a631c2ae24e4c26f
m3ndax <adrian.goessl@outlook.com>
2022-09-28 18:37:35 +0200

    update code and comment

--- random/acc_control_inheritance.cpp
+++ random/acc_control_inheritance.cpp
@@ -1,6 +1,10 @@
-#include <iostream.h>
+/*include the header file*/
+#include <iostream>
 #include <conio.h>
 
+using namespace std;
+
+/*student class read*/
 class student
 {
     public:
@@ -8,37 +12,43 @@ class student
     char name[20];
     void getdata()
     {
-        cout<<"Enter RollNo :-\t";
-        cin>>rno;
-        cout<<"Enter Name :-\t";
-        cin>>name;
+        /*get the data from the user*/
+        cout << "Enter RollNo :-\t";
+        cin >> rno;
+        cout << "Enter Name :-\t";
+        cin >> name;
     }
-
 };
+
+/*student class write*/
 class marks : public student
 {
-public:
+    public:
         int m1,m2,m3,tot;
         float per;
         void getmarks()
         {
+            /*get the marks from the user*/
             getdata();
-            cout<<"Enter Marks 1 :-\t";
-            cin>>m1;
-            cout<<"Enter Marks 2 :-\t";
-            cin>>m2;
-            cout<<"Enter Marks 3 :-\t";
-            cin>>m3;
+            cout << "Enter Marks 1 :-\t";
+            cin >> m1;
+            cout << "Enter Marks 2 :-\t";
+            cin >> m2;
+            cout << "Enter Marks 3 :-\t";
+            cin >> m3;
         }
+
+        /*function to calculate total and percentage*/
         void display()
         {
             getmarks();
-            cout<<"Roll Not \t Name \t Marks1 \t Marks2 \t Marks3 \t Total \t Percentage";
-
-            cout<<rno<<"\t"<<name<<"\t"<<m1<<"\t"<<m2<<"\t"<<m3<<"\t"<<tot<<"\t"<<per;
+            cout << "Roll Not \t Name \t Marks1 \t Marks2 \t Marks3 \t Total \t Percentage";
+            cout << rno<<"\t"<<name<<"\t"<<m1<<"\t"<<m2<<"\t"<<m3<<"\t"<<tot<<"\t"<<per;
         }
 };
-void main()
+
+/*main function*/
+int main()
 {
     student std;
     clrscr();
