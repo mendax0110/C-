e609e0b0010d8e43cd9b4a3ce2f3e1aab307bb0b
m3ndax <adrian.goessl@outlook.com>
2022-10-04 19:08:05 +0200

    update code and comment

--- random/LCM_HCF.cpp
+++ random/LCM_HCF.cpp
@@ -1,26 +1,40 @@
+// include the header file
 #include <iostream>
+
 using namespace std;
 
+// main function
 int main()
 {
     int numOne, numTwo, lcm, hcf, a, b, temp;
-    cout << "Enter Two Numbers: ";
+
+    // take input from user
+    cout << "Enter two numbers: ";
     cin >> numOne >> numTwo;
+
+    // assign the value of numOne and numTwo to a and b
     a = numOne;
     b = numTwo;
-        
-        while (!b = 0)
-        {
-            temp = b;
-            a = a % b;
-            a = temp;
-        }
 
-        hcf = a;
-        lcm = (numOne * numTwo) / hcf;
-        cout << "\nLCM ("<< numOne <<", "<< numTwo <<") = "<< lcm;
-        cout << "\nHCF ("<< numOne <<", "<< numTwo <<") = "<< hcf;
-        cout << endl;
+    // find the LCM
+    while (b != 0)
+    {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
 
-        return 0;
+    // assign the value of a to hcf
+    hcf = a;
+
+    // find the LCM
+    lcm = (numOne * numTwo) / hcf;
+
+    // print the LCM and HCF
+    cout << "\nLCM (" << numOne << ", " << numTwo << ") = " << lcm;
+    cout << "\nHCF (" << numOne << ", " << numTwo << ") = " << hcf;
+    cout << endl;
+
+    // return the value
+    return 0;
 }
