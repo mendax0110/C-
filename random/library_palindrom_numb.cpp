06f679edde496ceca358f7f9abdc185080b2e513
m3ndax <adrian.goessl@outlook.com>
2022-10-05 18:51:18 +0200

    update code and comment

--- random/library_palindrom_numb.cpp
+++ random/library_palindrom_numb.cpp
@@ -1,22 +1,34 @@
+// include the header files
 #include <iostream>
-#include <cstring>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
+// function to check if the number is palindrome or not
 int main()
 {
     int number, remainder;
-    cin >> number;
     
-    string s1 = to_string(number);  //storing string value in s1
-    string s2 = s1;                 //copy contents of s1
-    reverse(s2.begin(), s2.end());  //reversing the complete string s2
+    // take the input from the user
+    cout << "Enter a number: ";
+    cin >> number;
 
-    if (s1==s2)
-    cout << "palindrome";
+    // storing string value in s1
+    string s1 = to_string(number);
+    // copy contents of s1 to s2
+    string s2 = s1;
+    // reverse the string s2
+    reverse(s2.begin(), s2.end());
+
+    // check if s1 and s2 are equal
+    if(s1 == s2)
+        // if equal, then the number is palindrome
+        cout << "The number is a palindrome.";
     else
-    cout << "not a palindrome";
+        // if not equal, then the number is not palindrome
+        cout << "The number is not a palindrome.";
+    
+    // return 0 to indicate normal termination
     return 0;
 }
-
