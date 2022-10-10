9a5be22325c9d8b73c492f19c6fffbbb6871ae35
m3ndax <adrian.goessl@outlook.com>
2022-10-10 20:22:48 +0200

    update code and comment

--- random/root_cube_numb.cpp
+++ random/root_cube_numb.cpp
@@ -1,14 +1,22 @@
+// include the header file
 #include <iostream>
+
 using namespace std;
 
+// main function
 int main()
 {
     int i, n, cube;
-    cout << "Input the number of terms : ";
+
+    // take input from user
+    cout << "Enter the number of terms: ";
     cin >> n;
 
-    for (i = 1; i < n; i++) {
+    // loop to find the cube of the number
+    for(i = 1; i <= n; i++)
+    {
         cube = i * i * i;
-        cout << "Number is : " << "and its cube is : " << cube << endl;
+        // print the cube of the number
+        cout << "Number is: " << i << " and cube of the " << i << " is: " << cube << endl;
     }
 }
