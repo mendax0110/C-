97c1b267dc78bc64ab7d54fc96406dba2d4b1a61
m3ndax <adrian.goessl@outlook.com>
2022-10-18 21:43:36 +0200

    update code and comment

--- random/1x1.cpp
+++ random/1x1.cpp
@@ -1,15 +1,17 @@
-/*include the libraries*/
+// include the header file
 #include <iostream>
 
-/*main part of the program*/
+// main function
 int main()
 {
 	for(int i = 1; i <= 10; i++)
 	{
 		for(int i2 = 1; i2 <= 10; i2++)
 		{
-			std::cout << i << "x" << i2 << " = " << i*i2 << " ";
+			// print the number
+			std::cout << i << "x" << i2 << "=" << i*i2 << " ";
 		}
+		// print a new line
 		std::cout << std::endl << std::endl;
 	}
 }
