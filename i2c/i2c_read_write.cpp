b889514be047cd61c939d6d9df9c1936c16bd175
m3ndax <adrian.goessl@outlook.com>
2022-10-27 21:20:40 +0200

    update comment

--- i2c/i2c_read_write.cpp
+++ i2c/i2c_read_write.cpp
@@ -1,8 +1,10 @@
+// include the header file
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 
+// class for the i2c bus
 class i2cReadWrite
 {
     public:
@@ -15,6 +17,7 @@ class i2cReadWrite
         char *filename = id;
         if ((file_i2c = open(filename, O_RDWR)) < 0)
         {
+            // print error for failed i2c bus
             printf("Failed to open i2c bus");
             return;
         }
@@ -22,6 +25,7 @@ class i2cReadWrite
         int addr = 0x5a;
         if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
         {
+            // print error for false slave communication
             printf("Failed to acquire bus access and/or talk to slave.\n");
             return;
         }
@@ -31,20 +35,24 @@ class i2cReadWrite
     {
         if (read(file_i2c, buffer, length) != length)
         {
+            // print error for failed read
             printf("Failed to read from the i2c bus.\n");
         }
         else
         {
+            // print the data read
             printf("Data read: %s\n", buffer);
         }
         return res;
     }
+
     void writeBytes()
     {
         buffer[0] = 0x01;
         buffer[1] = 0x02;
         if (write(file_i2c, bufferm length) != length)
         {
+            // print error for failed write
             printf("Failed to write to the i2c bus.\n");
         }
     }
