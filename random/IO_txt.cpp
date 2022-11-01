5ded8d43c494d15fd0ede93cd16a4f0c7b5f05be
m3ndax <adrian.goessl@outlook.com>
2022-11-01 12:28:45 +0100

    update code and comment

--- random/IO_txt.cpp
+++ random/IO_txt.cpp
@@ -1,50 +1,62 @@
-/*include the header file*/
+// include the header file
 #include <iostream>
 #include <fstream>
 #include <string>
 
+// using standard namespace
 using namespace std;
 
-/*main function: create a stream object and open the file,
-*then read or write to the file
-*/
+/* main function: create a stream object and open the fiel
+ * read the file and print the content
+ * close the file
+ */
+
 int main()
 {
     string line;
 
-    /*create the output stream to write to the file
-     *append the new lines to the end of the file
+    /* create the output stream to write to the file
+     * append the new lines to the end of the file
      */
+
     ofstream myfile1("example.txt", ios::app);
 
+    // check if the file is open
     if(myfile1.is_open())
     {
+        // write to the file
         myfile1 << "This is the first Input for the 'example.txt' file.\n";
         myfile1 << "This is the second Input for the 'example.txt' file.\n";
         myfile1.close();
     }
     else
     {
-        cout << "Unable to open file";
+        // if the file is not open
+        cout << "Unable to open the file.\n";
     }
 
-    /*create the input stream to read from the file*/
-    ifstream myfile2("example.txt");
+    /* create the input stream to read from the file
+     * read the file line by line
+     */
+     ifstream myfile2("example.txt");
 
-    /*the file is open, the creating of ifstream takes place now*/
+     // if the file is open, the creating of ifstream object is successful
     if(myfile2.is_open())
     {
-        /*read the file line by line*/
+        // read the file line by line
         while(getline(myfile2, line))
         {
+            // print the content
             cout << line << endl;
         }
         myfile2.close();
     }
     else
     {
-        cout << "Unable to open file";
+        // if the file is not open
+        cout << "Unable to open the file.\n";
     }
 
+    // return 0
     return 0;
 }
