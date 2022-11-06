3af845b36d7db2bbb87b35803af716c39a555081
m3ndax <adrian.goessl@outlook.com>
2022-11-06 10:31:28 +0100

    update code and comment

--- binary/binary_decimal.cpp
+++ binary/binary_decimal.cpp
@@ -1,27 +1,33 @@
-/*include the libraries*/
+// include the libraries
 #include <iostream>
 #include <math.h>
 
+// use the standard namespace
 using namespace std;
 
+// main function
 int main()
 {
     long int i, no;
     int x, y = 0;
 
-    /*give the binary number*/
+    // get a binary number from the user
     cout << "Enter a binary number: ";
     cin >> no;
-    /*convert the binary number to decimal*/
-    cout << "\nThe decimal equivalent of " << no << " is: ";
 
+    // print the decimal number
+    cout << "\nThe decimal dequivalent of " << no << " is: ";
+
+    // cconvert the binary number to decimal
     for(i = 0; no != 0; i++)
     {
-        x = no % 10;
+        x %= 10;
         y = y + x * pow(2, i);
-        no = no / 10;
+        no /= 10;
     }
 
-    cout << y;
+    // print the decimal number
+    cout << y << endl;
+
     return 0;
 }
