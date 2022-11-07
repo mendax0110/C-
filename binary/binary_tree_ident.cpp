db829b8335fb4a7831c85e48abcdf2f311aae3dc
m3ndax <adrian.goessl@outlook.com>
2022-11-07 20:41:34 +0100

    update code and comment

--- binary/binary_tree_ident.cpp
+++ binary/binary_tree_ident.cpp
@@ -1,39 +1,63 @@
+// include the header file
 #include <bits/stdc++.h>
+
+// use the standard namespace
 using namespace std;
 
-struct Node{
+// define the node structure
+struct node
+{
     int data;
-    Node* left;
-    Node* right;
-    Node(int data)  {
+    node *left;
+    node *right;
+    node(int data)
+    {
         this->data = data;
-        left = right = NULL;
+        left = NULL;
+        right = NULL;
     }
 };
 
-bool isIdentical(Node *root1, Node *root2)  {
-    if(root1 == NULL && root2 == NULL) return true;
-    if((root1 && !root2) || (!root1 && root2)) return false;
-    if(root1->data != root2->data) return false;
-    return  isIdentical(root1->left, root2->left) && isIdentical(root1->right, root2->right);
+// function to check if two trees are identical
+bool isIdentical(node *root1, node *root2)
+{
+    // if both trees are empty
+    if (root1 == NULL && root2 == NULL)
+        return true;
+    // if both trees are non-empty
+    if (root1 != NULL && root2 != NULL)
+    {
+        // check if the data of both roots is same and data of left and right
+        // subtrees are also same
+        return (root1->data == root2->data && isIdentical(root1->left, root2->left) && isIdentical(root1->right, root2->right));
+    }
+    // if none of the above conditions is true then trees are not identical
+    return false;
 }
 
-int main()  {
- /*
-        1
-       / \
-      2   2
-     / \ / \
-    4  5 6  7
- */
-    struct Node* root = new Node(1);
-    root->left = new Node(2);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right = new Node(3);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
-    cout << isIdentical(root, root);
+// main function
+int main()
+{
+    // create the first tree
+    node *root1 = new node(1);
+    root1->left = new node(2);
+    root1->right = new node(3);
+    root1->left->left = new node(4);
+    root1->left->right = new node(5);
+
+    // create the second tree
+    node *root2 = new node(1);
+    root2->left = new node(2);
+    root2->right = new node(3);
+    root2->left->left = new node(4);
+    root2->left->right = new node(5);
+
+    // check if the trees are identical
+    if (isIdentical(root1, root2))
+        cout << "Both trees are identical";
+    else
+        cout << "Trees are not identical";
 
     return 0;
 }
+
