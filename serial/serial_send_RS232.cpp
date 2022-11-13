187f8d53f3bc59334a92b2bf536fb03643cd74a2
m3ndax <adrian.goessl@outlook.com>
2022-11-13 17:35:02 +0100

    update code and comment

--- serial/serial_send_RS232.cpp
+++ serial/serial_send_RS232.cpp
@@ -1,13 +1,23 @@
+/*include the header file*/
+#include <iostream>
+#include <windows.h>
+
+/*define the baudrate and delay time*/
 #define BAUD 9600
 #define DELAY_TIME 100
 
+/*define the serial port name*/
 const char* STUDYOS = "STUDY - OS";
 
-void setup() {
+/*define the serial port handle*/
+void setup() 
+{
     Serial.begin(BAUD);
 }
 
-void loop() {
+/*define the serial port send function*/
+void loop() 
+{
     Serial.println(STUDYOS);
     delay(DELAY_TIME);
 }
