69ad9a13e3d9967740ccbe230c60664a58930626
m3ndax <adrian.goessl@outlook.com>
2022-11-14 21:49:18 +0100

    update code and commet

--- serial/serial_usb_cpu.cpp
+++ serial/serial_usb_cpu.cpp
@@ -1,23 +1,28 @@
+// include the libraries
 #include "cpu.hpp"
 #include "util.hpp"
 #include "example/turing_machine.hpp"
 
+// main function
 int main()
 {
+    // create cpu, turing machine and serial
     using result = CPU < turing_machine > :: run;
     using printer = printer < result :: reg, result :: mem >;
 
-    if constexpr (result :: is_breakpoint)
+    if constexpr(result::is_breakpoint)
     {
-        std :: cout << "Stopped at brekpoint (pc =" << result :: pc <<")" << std :: endl;
+        std::cout << "Stopped at breakpoint (pc =" << result::pc << ")" << std::endl;
     }
 
-    std :: cout << "Executed " << result :: instr_count << "instructions\n" << std :: endl;
-    std :: cout << "Registers: " << std :: endl;
-    printer :: reg();
+    // print the registers and memory
+    std::cout << "Executed " << result::instr_count << "instructions\n" << std::endl;
+    std::cout << "Registers:\n" << printer::reg() << std::endl;
+    printer::reg();
 
-    std :: cout << "\nMemory:" << std :: endl;
-    printer :: mem();
+    // print the memory
+    std::cout << "Memory:\n" << printer::mem() << std::endl;
+    printer::mem();
 
     return 0;
 }
