bdaa9580fa9cfc3edb9c7c1ee4f13a38a84b45eb
m3ndax <adrian.goessl@outlook.com>
2022-11-27 11:43:44 +0100

    update code and comment

--- random/digits_count.cpp
+++ random/digits_count.cpp
@@ -1,24 +1,30 @@
-/*include the header file*/
+// include the header file
 #include <iostream>
 
+// use the standard namespace
 using namespace std;
 
-/*main function*/
+// main function
 int main()
 {
     int n, count = 0;
-    /*input the number*/
+
+    // get the number from the user
     cout << "Enter a number: ";
     cin >> n;
 
+    // loop until n becomes 0
     while (n != 0)
     {
+        // increase the count by 1
+        count++;
+
+        // remove the last digit from n
         n /= 10;
-        ++count;
     }
-    
-    /*print the number of digits*/
+
+    // print the number of digits
     cout << "Number of digits: " << count;
-    
+
     return 0;
 }
