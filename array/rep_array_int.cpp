a8e3f57b428924441754726e344ed602542e9599
m3ndax <adrian.goessl@outlook.com>
2022-11-28 21:43:38 +0100

    update comment

--- array/rep_array_int.cpp
+++ array/rep_array_int.cpp
@@ -1,25 +1,30 @@
-/*include the library*/
+// include the library
 #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-/*main function*/
+// main function
 int main()
 {
+    // declare the array
     int array[100], n, i;
 
+    // take the input
     cout << "Enter the number of elements in the array: ";
     cin >> n;
     cout << "\nEnter the elements of the array: ";
 
+    // add the elements to the array
     for(i = 0; i < n; i++)
     {
         cin >> array[i];
     }
 
+    // print the array
     cout << "Original array: ";
 
+    // now print the array
     for(int i = 0; i < n; i++)
     {
         cout << array[i] << " ";
