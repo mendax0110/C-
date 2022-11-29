d0dd41bab99231ac46de59c6fd98e32df2952520
m3ndax <adrian.goessl@outlook.com>
2022-11-29 22:13:13 +0100

    update code and comment

--- CPU/64_bit_cpu.cpp
+++ CPU/64_bit_cpu.cpp
@@ -1,46 +1,53 @@
-#include <iostream>
-#include <fstream>
+// include the header file
 #include <Windows.h>
-#include <vector>
-#include <sstream>
 #include <stdio.h>
-#include <stdarg.h>
-#include <string>
+#include <starg.h>
 #include <utils.h>
 #include <vm.h>
+#include <vector>
+#include <sstream>
+#include <string>
+
+// use the standard namespace
 using namespace std;
 
-//the CPUs main programm
-int main(int argc, char ** argv)
+// main function
+int main(int argc, char **argv)
 {
     bool verbose = false;
-    wstring script_path = Utils :: GetExecutablePath();
+    wstring script_path = Utils::GetExecutablePath();
 
-    for (int i = 0; i < argc; i++)
+    for(int i = 0; i < argc; i++)
     {
         string argument = argv[i];
-        if (argument == "--verbose")
+
+        if(argument == "--verbose")
         {
             verbose = true;
         }
+
         size_t ivyExt = argument.find(".ivy");
-        if (ivyExt != -1)
+        if(ivyExt != -1)
         {
             size_t isDrive = argument.find(":");
             wstring path(argument.begin(), argument.end());
-            if (isDrive != -1)
+
+            if(isDrive != -1)
             {
                 script_path = path;
             }
             else
             {
-                script_path = script_path + L"\\" + path;
+                script_path = Utils::GetExecutablePath() + L"\\" + path;
             }
         }
     }
 
-    VM * vm = new VM(script_path, verbose);
-    vm -> run();
-    SetConsoleTextAttribute(Utils :: hConsole, Utils :: CONSOLE_DEFAULT);
-    return 1;
+    // create a new VM
+    VM * vm = new VM(scipt_path, verbose);
+    vm->Run();
+    SetConsoleTextAttribute(Utils::hConsole, Utils::CONSOLE_DEFAULT);
+
+    return 0;
 }
+
