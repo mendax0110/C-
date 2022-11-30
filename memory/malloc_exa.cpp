8beb144eeec9520f2faf54dd080d006573ef3755
m3ndax <adrian.goessl@outlook.com>
2022-11-30 19:46:22 +0100

    update code and comment

--- memory/malloc_exa.cpp
+++ memory/malloc_exa.cpp
@@ -1,23 +1,35 @@
-/*include the header files*/
+// include the header files
 #include <stdio.h>
 #include <stdlib.h>
 
-/*function to allocate memory*/
+// function to allocate memory
 void func(int **b)
 {
-    *b  = (int*)malloc(sizeof(int));
+    // allocate memory dynamically
+    *b  = (int *)malloc(sizeof(int));
     **b = 10;
 
-    /*print the value of b*/
-    printf("%d\n", **b);
+    // print the value of b
+    printf("Value of b is %d\n", **b);
 }
 
-/*main function*/
+// main function
 int main()
 {
-    int **a = (int**)malloc(sizeof(int*));
+    // declare a pointer
+    int **a;
+
+    // allocate memory dynamically
+    a = (int **)malloc(sizeof(int *));
+
+    // call the function
     func(a);
 
-    /*print the value of a*/
-    printf("%d\n", **a);
+    // print the value of a
+    printf("Value of a is %d\n", **a);
+
+    // free the memory
+    free(*a);
+
+    return 0;
 }
